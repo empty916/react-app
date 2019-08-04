1041b18c1a89119ce92934c6825fceefa24adb60
empty916 <empty916@qq.com>
2019-08-04 21:43:50 +0800

    delete useless file

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,5 @@
 import createStore from './createStore';
-// import Provider from './Provider';
 import appState from '../App/state';
 import appActions from '../App/actions';
-
-// const store = createStore({appActions}, {appState});
 
 export default createStore({appActions}, {appState})
