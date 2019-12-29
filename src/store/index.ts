3930de5abdb455f5fe11224f94cc0e95d551f47f
empty916 <empty916@qq.com>
2019-12-29 16:41:41 +0800

    eslint bug fix

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,10 @@
 import {createStore} from 'rns-pure';
 import { promiseMiddleware, shallowEqualMiddleware } from 'rns-pure/dist/middlewares';
+import devTool from '@redux-devtool';
 import appState from '../App/state';
 import appActions from '../App/actions';
 import {location} from './route.store';
 import user from './user.store';
-import devTool from '@redux-devtool';
 import lazyModuleConfig from '../../server/autoGetModule/lazyLoadModuleConfig';
 
 const { modules: lazyModules } = lazyModuleConfig;
