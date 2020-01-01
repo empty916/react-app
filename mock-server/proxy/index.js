b650701ccd4c0fd138e9e269f84f0caa8514358c
empty916 <empty916@qq.com>
2020-01-01 19:45:41 +0800

    update eslint ignore

--- mock-server/proxy/index.js
+++ mock-server/proxy/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const proxy = require('http-proxy-middleware');
 const consoleStyle = require('./consoleStyle');
+
 const app = express();
 const config = require('./config');
 const urlList = require('./mock');
+
 const router = express.Router();
 
 const {
@@ -14,37 +16,37 @@ const {
 } = config;
 
 const commonProxy = proxy({
-    target: devApiUrl,
-    changeOrigin: true,
-    autoRewrite: true
+	target: devApiUrl,
+	changeOrigin: true,
+	autoRewrite: true,
 });
 const mockProxy = proxy({
-    target: 'http://localhost:7001',
-    changeOrigin: true,
+	target: 'http://localhost:7001',
+	changeOrigin: true,
 });
 
 /*
 请求处理接口
 处理代理、多个代理
 */
-router.use('/', function(req, res, next) {
-    if(urlList(req.url)){
-        console.log(consoleStyle.inverse, 'mock接口:' + req.url)
-        mockProxy(req, res, next);
-    } else {
-        commonProxy(req, res, next);
-    }
+router.use('/', (req, res, next) => {
+	if (urlList(req.url)) {
+		console.log(consoleStyle.inverse, `mock接口:${req.url}`);
+		mockProxy(req, res, next);
+	} else {
+		commonProxy(req, res, next);
+	}
 });
 
 app.use(baseUrl, router);
 
 
 app.listen(port, () => {
-    console.log(consoleStyle.green, '代理服务器已启动');
-    if (urlList.isMock) {
-        console.log(consoleStyle.blue, `当前在mock模式！`);
-    } else {
-        console.log(consoleStyle.magenta, `当前不在mock模式！`);
-    }
-    // console.log(consoleStyle.green, `cmd+单击链接(http://localhost:${port}), 打开页面`);
+	console.log(consoleStyle.green, '代理服务器已启动');
+	if (urlList.isMock) {
+		console.log(consoleStyle.blue, '当前在mock模式！');
+	} else {
+		console.log(consoleStyle.magenta, '当前不在mock模式！');
+	}
+	// console.log(consoleStyle.green, `cmd+单击链接(http://localhost:${port}), 打开页面`);
 });
