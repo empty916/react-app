53c5913d4697ef6fbcfc8225c139d7e29347357c
empty916 <empty916@qq.com>
2020-01-04 16:07:01 +0800

    复制粘帖脚本独立成npm包

--- server/create_module/index.js
+++ server/create_module/index.js
@@ -1,38 +1,18 @@
-// const path = require('path');
-const Check = require('./util/Check');
-const Create = require('./util/Create');
-// const ReadFile = require('./util/ReadFile');
-const Interactive = require('./util/Interactive');
-const { createFileNameQuestion } = require('./config');
+const CvScript = require('cv-script').default;
+const path = require('path');
+const { fileDataMap, moduleNameQuestion } = require('cv-script/dist/utils');
 
-// fileName == pageName
-const start = async () => {
-	const fileName = await Interactive.getUserInput(createFileNameQuestion);
+// 模板地址
+const templatePath = path.join(__dirname, 'templates');
+const distPath = path.join(__dirname, '..', '..', 'src', 'modules');
 
-	console.log('检查文件名...\n');
-	const fileNameIsValid = Check.nameIsValid(fileName);
 
-	if (!fileNameIsValid) {
-		console.log('请重新输入\n');
-		start();
-		return false;
-	}
+const cvs = new CvScript({
+	questions: [moduleNameQuestion],
+	templateDirPath: templatePath,
+	templateFilePath: templatePath,
+	distPath: distPath,
+	fileDataMaps: [fileDataMap],
+});
 
-	const templateType = await Interactive.getTemplate();
-
-	const create = new Create(fileName, templateType);
-
-	console.log('开始创建文件...\n');
-	create.createFiles();
-	console.log('创建文件成功!\n');
-	// console.log('开始链接redux数据\n');
-	// create.createReduxLink();
-	// console.log('redux数据链接成功！\n');
-	// console.log('开始导入到路由配置中...\n');
-	// create.createRouteLink();
-	// console.log('页面路由设置完成！\n');
-
-	console.log('模块创建成功！！！\n');
-};
-
-start();
+cvs.start();
