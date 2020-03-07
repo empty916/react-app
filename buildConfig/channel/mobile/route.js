752df7236609a115d9cf2584b9a759b06da24ceb
empty916 <empty916@qq.com>
2020-03-07 10:08:58 +0800

    升级natur

--- buildConfig/channel/mobile/route.js
+++ buildConfig/channel/mobile/route.js
@@ -1,19 +1,17 @@
 export default [
-    {
-        path: '/',
-        key: 'app',
-        breadcrumbName: '首页', // 面包屑的名字
-        component: require('@/app/index'),
-        indexRoute: {
-            breadcrumbName: 'Home',
-            component: require('@/pages/Home'),
-        },
-        childRoutes: [
-
-        ],
-    },
-    {
-        path: '/login',
-        component: require('@/layout/Login'),
-    }
+	{
+		path: '/',
+		key: 'app',
+		breadcrumbName: '首页', // 面包屑的名字
+		component: require('@/app/index'),
+		indexRoute: {
+			breadcrumbName: 'Home',
+			component: require('@/pages/Home'),
+		},
+		childRoutes: [],
+	},
+	{
+		path: '/login',
+		component: require('@/layout/Login'),
+	},
 ];
