9bbee26796158880b673ad6da10e1d4b3e3c71c8
empty916 <empty916@qq.com>
2020-04-04 14:25:43 +0800

    test component

--- src/modules/Page1/index.tsx
+++ src/modules/Page1/index.tsx
@@ -3,12 +3,30 @@ import Inject from '@inject';
 // import qs from 'query-string';
 import Input from '@base/Input';
 import { InjectStoreModule } from 'natur';
-import { Box } from '@material-ui/core';
+import { Box, GridList, GridListTile } from '@material-ui/core';
 
 const Page1: React.FC<{user: InjectStoreModule}> = ({user}) => (
-	<Box p={2} display='flex'>
-		<Input label='用户名' value={user.state.name} onChange={user.actions.updateName} />
-	</Box>
+	<>
+		<Box p={2} display='flex'>
+			<Box alignSelf='flex-start'>
+				<Input style={{height: 'auto'}} label='用户名' value={user.state.name} onChange={user.actions.updateName} />
+			</Box>
+			<Box flex='1' alignSelf='flex-start'>
+				<GridList cellHeight={50} cols={3}>
+					<GridListTile cols={2} rows={1}>
+						<img src="http://a3.att.hudong.com/14/75/01300000164186121366756803686.jpg" alt=""/>
+					</GridListTile>
+					<GridListTile cols={1} rows={1}>
+						<img src="http://a4.att.hudong.com/21/09/01200000026352136359091694357.jpg" alt=""/>
+					</GridListTile>
+					<GridListTile cols={3} rows={1}>
+						<img src="http://bbs.jooyoo.net/attachment/Mon_0905/24_65548_2835f8eaa933ff6.jpg" alt=""/>
+					</GridListTile>
+				</GridList>
+			</Box>
+		</Box>
+
+	</>
 );
 
 export {default as state} from './state';
