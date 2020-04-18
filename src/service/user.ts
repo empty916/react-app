84e91d3bbadb79e324e3c9d24e3fcdc2ad6a65ba
empty916 <empty916@qq.com>
2020-04-18 21:42:22 +0800

    更新natur-service

--- src/service/user.ts
+++ src/service/user.ts
@@ -1,6 +1,30 @@
 import store from '@/store';
 import { AuthType } from '@/constants/Auth';
+// import NaturService from 'natur-service';
 
 export const isLogin = () => store.getModule('user').maps.isLogin;
 
 export const hasAuth = (auth: string | undefined, authType?: AuthType) => store.getModule('user').maps.hasAuth(auth, authType);
+
+
+// class UserService extends NaturService {
+// 	constructor() {
+// 		super();
+// 		this.getModule('user');
+// 	}
+
+// 	fetchUserInfo() {
+// 		return this.user.actions.fetchData();
+// 	}
+
+// 	getName() {
+// 		return this.user.state.name;
+// 	}
+
+// 	removeName() {
+// 		return this.user.actions.removeName();
+// 	}
+// }
+
+
+// export default new UserService();
