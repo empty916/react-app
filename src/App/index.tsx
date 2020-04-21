e52f31ac6debd3f40139c05715d7fcfe309de068
empty916 <empty916@qq.com>
2020-04-21 10:09:48 +0800

    格式化代码

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { CssBaseline, Grid} from '@material-ui/core';
-import {ThemeProvider} from '@material-ui/core/styles';
+import { CssBaseline, Grid } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
 import materialTheme from '@/service/theme/material';
 import AuthRoute from '@/routes/AuthRoute';
 import AppMenu from '@biz/Menu';
@@ -10,19 +10,14 @@ import routes, { Index } from '@/routes';
 import '@/theme/test.scss';
 import styles from './style.scss';
 
-
-const f1 = {flex: 1};
+const f1 = { flex: 1 };
 
 const App: React.FC = () => (
 	<ThemeProvider theme={materialTheme}>
 		<CssBaseline />
-		<Grid container wrap='nowrap'>
+		<Grid container wrap="nowrap">
 			<AppMenu />
-			<Grid
-				item
-				style={f1}
-				className={styles.right}
-			>
+			<Grid item style={f1} className={styles.right}>
 				<Bar />
 				<Switch>
 					{routes.map((route: any, index) => (
