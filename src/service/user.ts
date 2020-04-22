525db3d3bae423ad17206d95d8698619a64651a6
empty916 <empty916@qq.com>
2020-04-22 11:22:12 +0800

    代码优化

--- src/service/user.ts
+++ src/service/user.ts
@@ -4,14 +4,10 @@ import NaturService from 'natur-service';
 class UserService extends NaturService {
 	constructor() {
 		super();
-		// this.getModule('user');
 		this.getModule('user');
-		this.getModule('page2');
-		this.watch('user', ({state}) => {
-			const res = this.dispatch('page2/changePageName', state.name);
-			// const res = this.page2?.actions?.changePageName(state.name);
-			res.then(console.log);
-		});
+		// this.watch('user', ({state}) => {
+		// 	this.dispatch('page2/changePageName', state.name);
+		// });
 	}
 
 	get isLogin() {
