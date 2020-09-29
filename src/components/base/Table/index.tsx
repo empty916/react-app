39df9ef42e50da7dccfc5b1be90a6d08d5ac0ed2
耿良祥 <gengliangxiang@gengliangxiangdeMacBook-Pro.local>
2020-09-29 15:26:19 +0800

    去除table警告

--- src/components/base/Table/index.tsx
+++ src/components/base/Table/index.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import {
-	TablePagination,
-} from '@material-ui/core';
 import MUIDataTable, { MUIDataTableProps } from 'mui-datatables';
+import TableFooter from '@material-ui/core/TableFooter';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import TablePagination from '@material-ui/core/TablePagination';
 
-
+type TableState = {
+	selectedRows: {
+		data: {dataIndex: number}[]
+	}
+};
 
 type TableProps = Omit<MUIDataTableProps, 'title'> & {
-    pagination: {
-        /** 总数据量 */
-        total: number;
-        /** 当前页数 */
-        page: number;
-        /** 每页的数据有多少行 */
-        rowsPerPage: number;
-    },
+	pagination: {
+		/** 总数据量 */
+		total: number;
+		/** 当前页数 */
+		page: number;
+		/** 每页的数据有多少行 */
+		rowsPerPage: number;
+	},
 	onPageChange?: (page: number) => void;
+	onChangeRowsPerPage?: (page: number) => void;
+	onTableChange?: (dataIndex: number[]) => void;
 	title?: string;
 };
 
-const Table: React.FC<TableProps> = ({options, data, pagination, onPageChange, title = '', ...restProps}) => {
+const Table: React.FC<TableProps> = ({options, data, pagination, onPageChange, onChangeRowsPerPage, onTableChange, title = '', ...restProps}) => {
 	const _options = React.useMemo(() => ({
 		filterType: 'checkbox' as 'checkbox',
 		jumpToPage: true,
@@ -43,7 +50,7 @@ const Table: React.FC<TableProps> = ({options, data, pagination, onPageChange, t
 			},
 			viewColumns: {
 				title: '显示列',
-				titleAria: '显示/隐藏 表哥列',
+				titleAria: '显示/隐藏 表格列',
 			},
 			pagination: {
 				next: '下一页',
@@ -60,20 +67,33 @@ const Table: React.FC<TableProps> = ({options, data, pagination, onPageChange, t
 			changeRowsPerPage: any,
 			changePage: any,
 		) => (
-			<TablePagination
-				rowsPerPageOptions={options?.rowsPerPageOptions || [5, 10, 15, 20, 25]}
-				component='div'
-				count={pagination.total}
-				rowsPerPage={pagination.rowsPerPage}
-				page={pagination.page - 1}
-				labelDisplayedRows={({ from, to, count: _count }) => `${from}-${to} 共 ${_count !== -1 ? _count : `more than ${to}`} 条数据`}
-				onChangePage={(event, np: number) => changePage(np)}
-				onChangeRowsPerPage={e => changeRowsPerPage(e.target.value)}
-			/>
+			<TableFooter>
+				<TableRow>
+					<TableCell>
+						<TablePagination
+							rowsPerPageOptions={options?.rowsPerPageOptions || [5, 10, 15, 20, 25]}
+							component='div'
+							count={pagination.total}
+							rowsPerPage={pagination.rowsPerPage}
+							page={pagination.page - 1}
+							labelDisplayedRows={({ from, to, count: _count }) => `${from}-${to} 共 ${_count !== -1 ? _count : `more than ${to}`} 条数据`}
+							onChangePage={(event, np: number) => changePage(np)}
+							onChangeRowsPerPage={e => changeRowsPerPage(e.target.value)}
+						/>
+					</TableCell>
+				</TableRow>
+			</TableFooter>
+
 		),
 		onChangePage: (np: number) => onPageChange && onPageChange(np + 1),
+		onChangeRowsPerPage: (size: number) => onChangeRowsPerPage && onChangeRowsPerPage(size),
+		onTableChange: (action: string, tableState: TableState) => {
+			if (onTableChange) {
+				return onTableChange(tableState.selectedRows.data.map(item => item.dataIndex));
+			}
+		},
 		...options,
-	}), [onPageChange, options, pagination.page, pagination.rowsPerPage, pagination.total]);
+	}), [onPageChange, onChangeRowsPerPage, onTableChange, options, pagination.page, pagination.rowsPerPage, pagination.total]);
 
 	return (
 		<MUIDataTable
